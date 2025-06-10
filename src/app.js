920e7241043caa92fec64af464011eb69a1840cf
kartikpandey0007 <kartikkpandey321@gmail.com>
2025-06-10 20:58:27 +0530

    exploring get ,post delete

--- src/app.js
+++ src/app.js
@@ -2,9 +2,37 @@ const express = require('express');
 
 const app = express()//calling express function(creating new express js appliaction)-> creating web server
 
-app.use("/hello",(req, res)=>{
-    res.send("hello hello hello")
+/*app.use("/",(req, res)=>{
+    res.send("hello from kartik")
+})*/
+
+/*app.use("/hello/2",(req, res)=>{
+    res.send("hello hello hello 2") 
+}) */  
+
+
+app.use("/user",(req, res)=>{
+    res.send("hahahaha") 
+}) 
+
+//this will only haldle GET call to /user
+app.get("/user", (req,res)=>{
+    res.send({firstName: "Kartik", lastName: "Pandey"})
 })
+
+app.post("/user", (req,res)=>{
+    //saving data to db
+    res.send("Data successfully saved to db")
+})
+
+app.delete("/user", (req,res)=>{
+    res.send("Deleted successfully")
+})
+
+//this will match all the HTTP methods API calls to /hello
+app.use("/hello",(req, res)=>{  
+    res.send("hello hello hello")
+}) 
 
 app.use("/test",(req, res)=>{
     res.send("Hello from server")
@@ -13,4 +41,3 @@ app.use("/test",(req, res)=>{
 app.listen(3000, ()=>{
     console.log("server is successfully running on port 3000...");//this will printed when server started succesfully
 })// if run,will wait to listen on port 3000
-console.log("yes")
