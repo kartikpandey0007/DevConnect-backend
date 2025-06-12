67db9359fd43c489cf2d93e0b5768c20b89d5248
kartikpandey0007 <kartikkpandey321@gmail.com>
2025-06-12 18:51:12 +0530

    dynamic data in api

--- src/app.js
+++ src/app.js
@@ -3,30 +3,19 @@ const connectDB = require("./config/database")
 const app = express()//calling express function(creating new express js appliaction)-> creating web server
 const User = require("./models/user")
 
-app.post("/signup", async (req,res)=>{
+app.use(express.json())
 
-    const user = new User({
-        firstName: "Kartik",
-        lastName: "Pandey",
-        emailId: "xyz@gamil.com",
-        password: "Ecci@1234",
-    });
+app.post("/signup", async (req, res) => {
+    
+    const user = new User(req.body);
 
-    try{
-        await user.save()
-        res.send("user added successfully")
+    try {
+        await user.save();
+        res.send("User added successfully");
     } catch (err) {
         res.status(400).send("Error saving the user: " + err.message);
     }
-})
-
- 
-
-
-
-
-
-
+});
 
 connectDB()
 .then(()=>{
