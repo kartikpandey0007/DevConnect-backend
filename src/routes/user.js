48131f8624ef825d5388e241aa89611485c725de
kartikpandey0007 <kartikkpandey321@gmail.com>
2025-07-04 16:16:33 +0530

    feed and pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const userRouter = express.Router();
 const  userAuth = require("../middlewares/auth");
-const ConnectionRequest = require("../models/ConnectionRequest");
+const ConnectionRequest = require("../models/connectionRequest");
 const User = require('../models/user');
 
 const USER_SAFE_DATA = "firstName lastName"
