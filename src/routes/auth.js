fb31f90ef03bb794013b1d3941e76eb127604cbe
kartikpandey0007 <kartikkpandey321@gmail.com>
2025-10-03 15:50:51 +0530

    fix2

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -75,7 +75,7 @@ authRouter.post("/login", async (req, res) => {
     res.status(400).send(err.message);
   }
 });
-
+//no change
 authRouter.post("/logout", async (req, res) => {
   try {
     res.cookie("token", null, { expires: new Date(Date.now()) });
